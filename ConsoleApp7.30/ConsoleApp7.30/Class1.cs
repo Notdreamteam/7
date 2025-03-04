32f0cd4724ebff502ff41da1453c745757f9603c
Notdreamteam <dtisckov@gmail.com>
2025-03-05 02:14:43 +0300

    Add files via upload

---
+++ ConsoleApp7.30/ConsoleApp7.30/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7._30
+{
+    class NumberChecker
+    {
+        public string CheckNumber(int number)
+        {
+            if (number % 2 == 0)
+            {
+                return "Число является четным.";
+            }
+            else if (number % 10 == 7)
+            {
+                return "Число оканчивается на 7.";
+            }
+            else
+            {
+                return "Число не является четным и не оканчивается на 7.";
+            }
+        }
+    }
+}
---
+++ ConsoleApp7.30/ConsoleApp7.30/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7._30
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите натуральное число: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            NumberChecker checker = new NumberChecker();
+
+            string result = checker.CheckNumber(number);
+
+            Console.WriteLine(result);
+        }
+    }
+}
